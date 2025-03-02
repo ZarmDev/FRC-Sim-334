597a46d84cb6587eaa236f62d412434ca9dfea61
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2025-03-02 12:02:57 -0500

    Added boost and other stuff

--- examples/example_win32_directx12/main.cpp
+++ examples/example_win32_directx12/main.cpp
@@ -6,24 +6,44 @@
 // - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
 // - Introduction, links and more at the top of imgui.cpp
 
-#include <winsock2.h>
-#include <ws2tcpip.h>
+// Sockets
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+// IMGUI
 #include "imgui.h"
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx12.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <tchar.h>
+
+// Standard library
 #include <string>
 #include <iostream>
-#include <boost/asio.hpp>
-#include <boost/beast.hpp>
+#include <random>
+#include <sstream>
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+// WINRT (socket programming)
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Networking.Sockets.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+// BOOST
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+
+using namespace winrt;
+using namespace Windows::Networking::Sockets;
+using namespace Windows::Storage::Streams;
+using namespace Windows::Foundation::Collections;
+
+//namespace beast = boost::beast;     // from <boost/beast.hpp>
+//namespace http = beast::http;       // from <boost/beast/http.hpp>
+//namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+//namespace net = boost::asio;        // from <boost/asio.hpp>
+//using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -119,6 +139,20 @@ enum windows {
     Demo,
     Test
 };
+struct TopicData {
+    int topicID;
+    int64_t timestamp_us;
+    int typeIdx;
+    std::string value;
+
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar& topicID;
+        ar& timestamp_us;
+        ar& typeIdx;
+        ar& value;
+    }
+};
 
 // Forward declarations of helper functions
 bool CreateDeviceD3D(HWND hWnd);
@@ -128,6 +162,110 @@ void CleanupRenderTarget();
 void WaitForLastSubmittedFrame();
 FrameContext* WaitForNextFrameResources();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//void handleJsonMessage(const std::string& message) {
+//    try {
+//        auto rxArray = json::parse(message);
+//
+//        for (const auto& msg : rxArray) {
+//            if (!msg.is_object()) {
+//                std::cout << "[NT4] Ignoring text message, JSON parsing did not produce an object." << std::endl;
+//                continue;
+//            }
+//
+//            if (!msg.contains("method") || !msg.contains("params")) {
+//                std::cout << "[NT4] Ignoring text message, JSON parsing did not find all required fields." << std::endl;
+//                continue;
+//            }
+//
+//            std::string method = msg["method"];
+//            auto params = msg["params"];
+//
+//            if (!params.is_object()) {
+//                std::cout << "[NT4] Ignoring text message, JSON parsing found \"params\", but it wasn't an object." << std::endl;
+//                continue;
+//            }
+//
+//            if (method == "announce") {
+//                // Handle announce method
+//                std::cout << "[NT4] Announce: " << params.dump() << std::endl;
+//            }
+//            else if (method == "unannounce") {
+//                // Handle unannounce method
+//                std::cout << "[NT4] Unannounce: " << params.dump() << std::endl;
+//            }
+//            else if (method == "properties") {
+//                // Handle properties method
+//                std::cout << "[NT4] Properties: " << params.dump() << std::endl;
+//            }
+//            else {
+//                std::cout << "[NT4] Ignoring text message - unknown method " << method << std::endl;
+//            }
+//        }
+//    }
+//    catch (const std::exception& ex) {
+//        std::cerr << "[NT4] JSON parsing error: " << ex.what() << std::endl;
+//    }
+//}
+
+//void handleMsgPackMessage(const std::vector<uint8_t>& data) {
+//    try {
+//        msgpack::object_handle oh = msgpack::unpack(reinterpret_cast<const char*>(data.data()), data.size());
+//        msgpack::object obj = oh.get();
+//        std::vector<msgpack::object> rxArray;
+//        obj.convert(rxArray);
+//
+//        for (const auto& unpackedData : rxArray) {
+//            int topicID = unpackedData.via.array.ptr[0].as<int>();
+//            int64_t timestamp_us = unpackedData.via.array.ptr[1].as<int64_t>();
+//            int typeIdx = unpackedData.via.array.ptr[2].as<int>();
+//            auto value = unpackedData.via.array.ptr[3];
+//
+//            if (topicID >= 0) {
+//                // Handle new topic data
+//                std::cout << "[NT4] New topic data: " << topicID << ", " << timestamp_us << ", " << typeIdx << ", " << value << std::endl;
+//            }
+//            else if (topicID == -1) {
+//                // Handle receive timestamp
+//                std::cout << "[NT4] Receive timestamp: " << timestamp_us << ", " << value << std::endl;
+//            }
+//            else {
+//                std::cout << "[NT4] Ignoring binary data - invalid topic id " << topicID << std::endl;
+//            }
+//        }
+//    }
+//    catch (const std::exception& ex) {
+//        std::cerr << "[NT4] MessagePack parsing error: " << ex.what() << std::endl;
+//    }
+//}
+
+void handleBinaryMessage(const std::vector<uint8_t>& data) {
+    try {
+        std::stringstream ss;
+        ss.write(reinterpret_cast<const char*>(data.data()), data.size());
+        boost::archive::binary_iarchive ia(ss);
+
+        std::vector<TopicData> rxArray;
+        ia >> rxArray;
+
+        for (const auto& unpackedData : rxArray) {
+            if (unpackedData.topicID >= 0) {
+                // Handle new topic data
+                std::cout << "[NT4] New topic data: " << unpackedData.topicID << ", " << unpackedData.timestamp_us << ", " << unpackedData.typeIdx << ", " << unpackedData.value << std::endl;
+            }
+            else if (unpackedData.topicID == -1) {
+                // Handle receive timestamp
+                std::cout << "[NT4] Receive timestamp: " << unpackedData.timestamp_us << ", " << unpackedData.value << std::endl;
+            }
+            else {
+                std::cout << "[NT4] Ignoring binary data - invalid topic id " << unpackedData.topicID << std::endl;
+            }
+        }
+    }
+    catch (const std::exception& ex) {
+        std::cerr << "[NT4] Binary parsing error: " << ex.what() << std::endl;
+    }
+}
 
 // Main code
 int main(int, char**)
@@ -196,57 +334,172 @@ int main(int, char**)
     // Our state 
     windows currentWindow = windows::Demo;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    // The io_context is required for all I/O
-    net::io_context ioc;
 
-    // These objects perform our I/O
-    tcp::resolver resolver(ioc);
-    websocket::stream<tcp::socket> ws(ioc);
-    // This buffer will hold the incoming message
-    beast::flat_buffer buffer;
+    //WSADATA wsaData;
+    //SOCKET ConnectSocket = INVALID_SOCKET;
+    //struct addrinfo* result = NULL, * ptr = NULL, hints;
+    //const char* sendbuf = "Hello from client";
+    //char recvbuf[512];
+    //int iResult;
+    //int recvbuflen = 512;
 
-    try {
-        // Look up the domain name
-        auto const results = resolver.resolve(SIM_ADDRESS, PORT);
+    //// Initialize Winsock
+    //iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    //if (iResult != 0) {
+    //    std::cerr << "WSAStartup failed: " << iResult << std::endl;
+    //    return 1;
+    //}
 
-        // Make the connection on the IP address we get from a lookup
-        net::connect(ws.next_layer(), results.begin(), results.end());
-        cout << "1";
+    //ZeroMemory(&hints, sizeof(hints));
+    //hints.ai_family = AF_UNSPEC;
+    //hints.ai_socktype = SOCK_STREAM;
+    //hints.ai_protocol = IPPROTO_TCP;
 
-        // Perform the WebSocket handshake
-        ws.handshake(SIM_ADDRESS + ":" + PORT, "/");
-        cout << "2";
+    //// Resolve the server address and port
+    //iResult = getaddrinfo(SIM_ADDRESS, PORT, &hints, &result);
+    //if (iResult != 0) {
+    //    std::cerr << "getaddrinfo failed: " << iResult << std::endl;
+    //    WSACleanup();
+    //    return 1;
+    //}
 
-        // Send a message
-        //ws.write(net::buffer(std::string("Hello, WebSocket!")));
+    //// Attempt to connect to an address until one succeeds
+    //for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+    //    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    //    if (ConnectSocket == INVALID_SOCKET) {
+    //        std::cerr << "Error at socket(): " << WSAGetLastError() << std::endl;
+    //        WSACleanup();
+    //        return 1;
+    //    }
 
-        // Read a message into our buffer
-        ws.read(buffer);
-    }
-    catch (beast::system_error const& se) {
-        if (se.code() == websocket::error::closed) {
-            std::cerr << "WebSocket connection closed: " << se.what() << std::endl;
+    //    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    //    if (iResult == SOCKET_ERROR) {
+    //        closesocket(ConnectSocket);
+    //        ConnectSocket = INVALID_SOCKET;
+    //        continue;
+    //    }
+    //    break;
+    //}
+
+    //freeaddrinfo(result);
+
+    //if (ConnectSocket == INVALID_SOCKET) {
+    //    std::cerr << "Unable to connect to server!" << std::endl;
+    //    WSACleanup();
+    //    return 1;
+    //}
+
+    //// Send an initial buffer
+    //iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    //if (iResult == SOCKET_ERROR) {
+    //    std::cerr << "send failed: " << WSAGetLastError() << std::endl;
+    //    closesocket(ConnectSocket);
+    //    WSACleanup();
+    //    return 1;
+    //}
+
+    //std::cout << "Bytes Sent: " << iResult << std::endl;
+
+    //try {
+    //    // The io_context is required for all I/O
+    //    net::io_context ioc;
+
+    //    // These objects perform our I/O
+    //    tcp::resolver resolver(ioc);
+    //    websocket::stream<tcp::socket> ws(ioc);
+
+    //    // Look up the domain name
+    //    auto const results = resolver.resolve("echo.websocket.org", "80");
+
+    //    // Make the connection on the IP address we get from a lookup
+    //    net::connect(ws.next_layer(), results.begin(), results.end());
+
+    //    // Perform the WebSocket handshake
+    //    ws.handshake("echo.websocket.org", "/");
+
+    //    // Send a message
+    //    ws.write(net::buffer(std::string("Hello, WebSocket!")));
+
+    //    // This buffer will hold the incoming message
+    //    beast::flat_buffer buffer;
+
+    //    // Read a message into our buffer
+    //    ws.read(buffer);
+
+    //    // Print the message
+    //    std::cout << beast::make_printable(buffer.data()) << std::endl;
+
+    //    // Close the WebSocket connection
+    //    ws.close(websocket::close_code::normal);
+    //}
+    //catch (std::exception const& e) {
+    //    std::cerr << "Error: " << e.what() << std::endl;
+    //    return EXIT_FAILURE;
+    //}
+    init_apartment();
+
+    // Generate UUID
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1, 9999999999);
+    // TODO: temporary
+    int UUID = distribution(generator);
+
+    // Important variables
+    std::string serverBaseAddr = "127.0.0.1";
+    int port = 5810;
+    std::string prefix = "ws://";
+    std::ostringstream oss;
+    oss << prefix << serverBaseAddr << ":" << port << "/nt/FRC-sim_" << UUID;
+    std::string serverAddr = oss.str();
+
+    // Create a MessageWebSocket object
+    MessageWebSocket webSocket;
+
+    // Set the protocol for the WebSocket connection
+    webSocket.Control().SupportedProtocols().Append(L"v4.1.networktables.first.wpi.edu");
+
+    // Attach the MessageReceived event handler
+    webSocket.MessageReceived([](MessageWebSocket const&, MessageWebSocketMessageReceivedEventArgs const& args) {
+        DataReader reader = args.GetDataReader();
+        reader.UnicodeEncoding(UnicodeEncoding::Utf8);
+        auto message = reader.ReadString(reader.UnconsumedBufferLength());
+        std::string messageStr = winrt::to_string(message);
+        cout << messageStr;
+
+        if (messageStr[0] == '{') {
+            // Handle JSON message
+            //handleJsonMessage(messageStr);
         }
         else {
-            std::cerr << "Error: " << se.what() << std::endl;
+            // Handle binary message
+            std::vector<uint8_t> data(messageStr.begin(), messageStr.end());
+            handleBinaryMessage(data);
         }
-    }
-    catch (std::exception const& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+        });
 
+    // Connect to the WebSocket server
     try {
-        // Print the message
-        std::cout << beast::make_printable(buffer.data()) << std::endl;
+        webSocket.ConnectAsync(winrt::Windows::Foundation::Uri(winrt::to_hstring(serverAddr))).get();
+        std::wcout << L"Connected to WebSocket server!" << std::endl;
+
+        // Send a test message to the server
+        DataWriter writer(webSocket.OutputStream());
+        writer.WriteString(L"Hello, server!");
+        writer.StoreAsync().get();
+        std::wcout << L"Test message sent to server." << std::endl;
+
+        // Keep the application running to receive messages
+        std::cin.get();
     }
-    catch (std::exception const& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+    catch (const winrt::hresult_error& ex) {
+        std::wcerr << L"WebSocket connection failed: " << ex.message().c_str() << std::endl;
+        std::wcerr << L"HRESULT: " << std::hex << ex.code() << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::wcerr << L"WebSocket connection failed: " << ex.what() << std::endl;
     }
 
-    // Close the WebSocket connection
-    //ws.close(websocket::close_code::normal);
+    return 0;
 
     // Main loop
     bool done = false;
